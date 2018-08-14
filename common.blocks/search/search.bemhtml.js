a2ea1910cf7e183d9b984e2ac01a9772da9374e9
KMishik <k.mishik@gmail.com>
2018-08-14 22:53:11 +0500

    Finish header modeling

--- common.blocks/search/search.bemhtml.js
+++ common.blocks/search/search.bemhtml.js
@@ -1,3 +1,12 @@
 block('search')({
   tag: 'form'
 });
+block('search').elem('input')({
+  tag: 'input',
+  js: true,
+  attrs: {
+    type: 'text',
+    name: 'regexp',
+    placeholder: 'Найти...'
+  }
+});
